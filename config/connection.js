476f6fe8fec2692f19d05363e5d2072e3dc6bb93
Ashley Whaley <73979892+AshleyWhaley@users.noreply.github.com>
2021-03-30 20:08:09 -0700

    adding connection and schema

---
+++ config/connection.js
@@ -0,0 +1,24 @@
+//Import dependencies
+const Sequelize = require('sequelize');
+require('dotenv').config();
+
+let sequelize;
+
+//Set up sequelize
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306
+    }
+  );
+}
+
+//Export sequelize
+module.exports = sequelize;
