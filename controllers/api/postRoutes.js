ac078b173ae692c8fa56d8e5838ab25e7db61ebf
Ashley Whaley <73979892+AshleyWhaley@users.noreply.github.com>
2021-05-25 18:32:03 -0700

    post route

---
+++ controllers/api/postRoutes.js
@@ -0,0 +1,59 @@
+//Import dependencies
+const router = require('express').Router();
+const { Post } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+//Create post
+router.post('/', withAuth, async (req, res) => {
+    try {
+        const postData = await Post.create ({
+        ...req.body,
+        user_id: req.session.user_id,
+        });
+
+        res.status(200).json(postData);
+
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+//Update post
+router.put('/:id', withAuth, async (req, res) => {
+    try {
+        const postData = await Post.update ({
+            title: req.body.title,
+            content: req.body.content,
+        },
+            {
+                where: {
+                    id: req.params.id
+                }
+            });
+
+        res.status(200).json(postData);
+
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+//Delete  post
+router.delete('/:id', withAuth, async (req, res) => {
+    try {
+        await Post.destroy ({
+            where: {
+                id: req.params.id,
+                user_id: req.session.user_id,
+            }
+        });
+
+        res.status(204).end();
+
+    } catch (err) {
+        console.log(err);
+        res.status(400).json(err);
+    }
+});
+
+module.exports = router;
