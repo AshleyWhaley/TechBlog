d6d84c2023c2638b080becb9aa2fb9b91d625467
Ashley Whaley <73979892+AshleyWhaley@users.noreply.github.com>
2021-05-25 18:29:03 -0700

    setting out routes and dependencies

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -4,10 +4,12 @@ const router = require('express').Router();
 //importing files
 const commentRoutes = require('./comment-routes');
 const userRoutes = require('./user-routes');
+const postRoutes = require('./postRoutes');
 
 //configure routes
 router.use("/comment", commentRoutes);
 router.use("/user", userRoutes);
+router.use('/posts', postRoutes);
 
 //export routes
 module.exports = router;
